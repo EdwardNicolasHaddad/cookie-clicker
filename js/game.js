6583f737e847f8e18bb7ab3972b1d4df82170830
EdwardNicolasHaddad <eeex1x2@gmail.com>
2026-07-16 18:44:23 +0200

    Add cookie click event to track crumbs

--- js/game.js
+++ js/game.js
@@ -1,1 +1,10 @@
+let crumbs = 0;
 
+const cookie = document.querySelector("button");
+const crumbDisplay = document.querySelector("h2");
+
+cookie.addEventListener("click", function() {
+    crumbs++;
+
+    crumbDisplay.textContent = "Krümel: " + crumbs;
+});
