e4206b5136301ee432e83b644842a91dafd39d06
EdwardNicolasHaddad <eeex1x2@gmail.com>
2026-07-16 18:51:02 +0200

    Load crumbs from localStorage and update display
    Initialize crumbs from localStorage and update display on button click.

--- js/game.js
+++ js/game.js
@@ -1,10 +1,16 @@
-let crumbs = 0;
+let crumbs = localStorage.getItem("crumbs") || 0;
 
 const cookie = document.querySelector("button");
 const crumbDisplay = document.querySelector("h2");
+
+crumbs = Number(crumbs);
+
+crumbDisplay.textContent = "Krümel: " + crumbs;
 
 cookie.addEventListener("click", function() {
     crumbs++;
 
     crumbDisplay.textContent = "Krümel: " + crumbs;
+
+    localStorage.setItem("crumbs", crumbs);
 });
