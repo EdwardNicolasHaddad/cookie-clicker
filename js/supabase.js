410432411cca5e70b0f0e438f531a3d3f0ecb62c
EdwardNicolasHaddad <eeex1x2@gmail.com>
2026-07-16 19:09:59 +0200

    Initialize Supabase client with URL and key

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,7 @@
+const supabaseUrl = "https://anezvycsuuawslymtaji.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFuZXp2eWNzdXVhd3NseW10YWppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMTQxNzgsImV4cCI6MjA5OTc5MDE3OH0.2OSb_aDNEPSMYHpGaNcQ1bkZbGYB84M1odHwlQnn3E0";
 
+const supabaseClient = supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
